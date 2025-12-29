27bebe3b4c41123a70924132a157b366b282ed73
victorxx <victorsaib@gmail.com>
2025-12-29 11:30:00 -0300

    Add PHP script to replace 'PHP' with 'PYHON'

---
+++ contandonumerovezesmudanca.php
@@ -0,0 +1,10 @@
+<?php
+$texto='PHP É MUITO DIVERTIDO PHP PODERIA SER MELHOR';
+
+
+$cont=0;
+$mudar=str_replace('PHP','PYHON',$texto,$cont);
+echo $mudar;
+echo'<br><br><br>';
+echo" trocamos $cont  vezes na expressão a cima ";
+?>
