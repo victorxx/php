4ce1be0f03cedec90f218f1f641fee5dabc90d4c
victorxx <victorsaib@gmail.com>
2026-01-01 23:08:13 -0300

    Create criarcookie.php

---
+++ criarcookie.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+
+    </head>
+    <body>
+        <?php
+        $conteudo;
+        ?>
+    <script>
+        document.cookie='idade=menor;path=/;max-age='+<?php $conteudo?>
+        alert('cookie criado');
+    </script>
+    </body>
+</html>
