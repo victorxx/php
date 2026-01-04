fd258ce718ffc421ec80e455c390afab9f2eec2c
victorxx <victorsaib@gmail.com>
2026-01-04 02:05:04 -0300

    Create pregmatchvarios.php

---
+++ pregmatchvarios.php
@@ -0,0 +1,10 @@
+
+
+<?php
+$animal="os animais cachorro papagaio são muito fofos igual os gatos";
+if(preg_match("/cachorro||gato/i",$animal)){
+    echo "achou um animal muito bom<br>";
+
+}
+
+?>
