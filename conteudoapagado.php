1360d52b3b1c5065e79ed9fdccc51b3cb99f3eeb
victorxx <victorsaib@gmail.com>
2026-01-04 02:30:07 -0300

    Create conteudoapagado.php

---
+++ conteudoapagado.php
@@ -0,0 +1,22 @@
+<?php
+$arquivo="pasta/ok.txt";
+if(file_exists($arquivo)){
+    $linhas=file($arquivo);
+    $achou=false;
+
+    foreach($linhas as $linha){
+        if(preg_match("/victor/i",$linha)){
+            $achou=true;
+            break;
+        }
+    }
+   
+}
+if($achou){
+    file_put_contents($arquivo,"");
+    echo"conteudo apagado";
+}else{
+    echo "ok";
+}
+
+?>
