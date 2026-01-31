141ae4d612bd6ce13397405f5a58995cf6c7f964
victorxx <victorsaib@gmail.com>
2026-01-31 17:11:30 -0300

    Create diferenca==e===no.php

---
+++ diferenca==e===no.php
@@ -0,0 +1,11 @@
+<?php
+$valor=0;
+if($valor==false)
+    {
+        echo "com == entrou porque é considerado falso";
+    }
+    if($valor===false)
+        {
+            echo "não entrou porque 0 não e bolleano";
+        }
+?>
